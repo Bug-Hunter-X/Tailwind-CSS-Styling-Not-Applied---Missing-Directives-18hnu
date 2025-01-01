b33d555555aebf9034a68ce8c3d0bc2f7d65f217
BugHunter-X <askzenai@gmail.com>
2025-01-01 11:28:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="bg-gray-300 p-4 rounded-lg shadow-md">
+  <p>This is some text.</p>
+</div>
+```
+This code snippet uses Tailwind CSS classes to style a div element. However, if you don't include the necessary Tailwind directives in your CSS, you might not see the expected styling applied.
